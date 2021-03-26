bbdd701d06ea08cdd56993677fcf7aa83b8c4248
AMD-CO <77602404+AMD-CO@users.noreply.github.com>
2021-03-26 14:38:44 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"605d9ed63022f1001e5cee55",
+				"605d9ed63022f1001e5cee57"
+			]
+		},
+		"605d9ed63022f1001e5cee55": {
+			"id": "605d9ed63022f1001e5cee55",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"605d9ed63022f1001e5cee57": {
+			"id": "605d9ed63022f1001e5cee57",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
